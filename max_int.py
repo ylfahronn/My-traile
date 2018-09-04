ecd55735ae3ccf9cb4a8868d5542bc8c3186145c
Ylfa Ásbjörnsdóttir <ylfaasbjornsdottir@Ylfas-MacBook-Pro.local>
2018-09-04 11:54:25 +0000

    Algorithim written for max_int.py

--- max_int.py
+++ max_int.py
@@ -1,14 +1,15 @@
 #max int = 0
 #num_int = input a value
 # count_int -= 1 will lower the number
-#sum_int = 0 will asign the 0 to the sum
+#max_int = 0 will asign the 0 to the sum
+# Do not change this line
 
-
-num_int = int(input("Input a number: "))    # Do not change this line
-max_int = 
+num_int = int(input("Input a number: "))
+max_int = 0
 
 while num_int >= 0:
-    num_int -= 1# Fill in the missing code
-    
+    num_int = int(input("Input a number: ")) # Fill in the missing code
+    if num_int > max_int:
+        max_int = num_int
 
 print("The maximum is", max_int)    # Do not change this line
