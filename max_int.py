a7c2bdf5993dc3ea20a1e4ac3d2578b8e771d12d
Ylfa Ásbjörnsdóttir <ylfaasbjornsdottir@Ylfas-MacBook-Pro.local>
2018-09-04 11:38:40 +0000

    algorithm written for max_int.py

---
+++ max_int.py
@@ -0,0 +1,14 @@
+#max int = 0
+#num_int = input a value
+# count_int -= 1 will lower the number
+#sum_int = 0 will asign the 0 to the sum
+
+
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 
+
+while num_int >= 0:
+    num_int -= 1# Fill in the missing code
+    
+
+print("The maximum is", max_int)    # Do not change this line
